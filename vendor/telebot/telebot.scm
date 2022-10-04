c17e701cdd2e062698b73bf66fde354ad4b1fe22
khorsun-d22 <113513035+khorsun-d22@users.noreply.github.com>
2022-10-04 20:43:56 +0000

    Add send-poll

--- vendor/telebot/telebot.scm
+++ vendor/telebot/telebot.scm
@@ -198,6 +198,26 @@
                              reply_to_message_id
                              reply_markup))
 
+  (wrap-api-method "sendPoll"
+                   semd-poll
+                   (required question
+                             options)
+                   (optional is_anonymous
+                             type
+                             allows_multiple_answers
+                             correct_option_id
+                             explanation
+                             explanation_parse_mode
+                             explanation_entities
+                             open_period
+                             close_date
+                             is_closed
+                             disable_notification
+                             protect_content
+                             reply_to_message_id
+                             allow_sending_without_reply
+                             reply_markup))
+
   (wrap-api-method "sendChatAction"
 		   send-chat-action
                    (required chat_id
